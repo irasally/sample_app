e1a15fe659ba7e388a78af7d0e06941d6ca259f6
irasally <irasally@gmail.com>
2013-02-04 17:10:11 +0900

    [#43743377] hello#index はイケてない

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 SampleApp::Application.routes.draw do
   root :to => 'hello#index'
-  get "hello/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
